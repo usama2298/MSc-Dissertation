5e1a14f0cc1405743bfe897e778a933df824148f
usama2298 <usamaahmad713@gmail.com>
2021-09-29 14:16:39 +0100

    Splits into Train and Test Data, Predictions on Test Data, CSV flies of Train and Test Data

--- Sample Analysis 2.R
+++ Sample Analysis 2.R
@@ -1,54 +1,34 @@
-#Multinomial Analysis by putting Underweight as Normal.
-Obesity2$BMIgroup <- ifelse(Obesity2$BMIgroup == "Underweight", "Normal",Obesity2$BMIgroup)
-Obesity2$AgeGroup <- as.factor(Obesity2$AgeGroup)
-Obesity2$Sex <- as.factor(Obesity2$Sex)
-Obesity2$Employment <- as.factor(Obesity2$Employment)
-Obesity2$Veg <- as.factor(Obesity2$Veg)
-Obesity2$Fruit <- as.factor(Obesity2$Fruit)
-Obesity2$BMIgroup <- as.factor(Obesity2$BMIgroup)
-Obesity2$Obese <- as.factor(if_else(Obesity2$BMIgroup == "Obese", "Yes", "No"))
 
-multi_mod1_1 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit + Veg, data = Obesity2)
-mod <- step(multi_mod1_1)
-multi_mod2_2 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit, data = Obesity2)
-multi_mod3_3 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity2)
-multi_mod4_4 <- multinom(BMIgroup ~ AgeGroup + Sex, data = Obesity2)
-multi_mod5_5 <- multinom(BMIgroup ~ AgeGroup, data = Obesity2)
+Obesity_num <- Obesity
+
+# Multinomial Analysis by putting Underweight as Normal
+Obesity_num$BMIgroup <- ifelse(Obesity_num$BMIgroup == "Underweight", "Normal", Obesity_num$BMIgroup)
+Obesity_num$AgeGroup <- as.factor(Obesity_num$AgeGroup)
+Obesity_num$Sex <- as.factor(Obesity_num$Sex)
+Obesity_num$Employment <- as.factor(Obesity_num$Employment)
+Obesity_num$Veg <- as.factor(Obesity_num$Veg)
+Obesity_num$Fruit <- as.factor(Obesity_num$Fruit)
+Obesity_num$BMIgroup <- as.factor(Obesity_num$BMIgroup)
+Obesity_num$Obese <- as.factor(if_else(Obesity_num$BMIgroup == "Obese", "Yes", "No"))
+
+multi_mod1_1 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit + Veg, data = Obesity_num)
+mod <- step(multi_mod1_1) ; rm(mod)
+multi_mod2_2 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit, data = Obesity_num)
+multi_mod3_3 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity_num)
+multi_mod4_4 <- multinom(BMIgroup ~ AgeGroup + Sex, data = Obesity_num)
+multi_mod5_5 <- multinom(BMIgroup ~ AgeGroup, data = Obesity_num)
 
 # Predictions by Different Models
-Predictions2 <- data.frame(True_Values = Obesity2$BMIgroup, Multi_Mod1_1 = predict(multi_mod3_3),
+Predictions2 <- data.frame(True_Values = Obesity_num$BMIgroup, Multi_Mod1_1 = predict(multi_mod3_3),
                            Multi_Mod2_2 = predict(multi_mod2_2), Multi_Mod3_3 = predict(multi_mod3_3),
                            Multi_Mod4_4 = predict(multi_mod4_4), Multi_Mod5_5 = predict(multi_mod5_5))
 
 # Correct Classification rate
-CP <- xtabs( ~ Predictions2$True_Values + Predictions2$Multi_Mod2_2)
-(CP[1,1]+CP[2,2]+CP[3,3]+CP[4,4])/nrow(Obesity2)
+CP2 <- xtabs( ~ Predictions2$True_Values + Predictions2$Multi_Mod2_2) ; CP2
+(CP2[1,1]+CP2[2,2]+CP2[3,3]+CP2[4,4])/nrow(Obesity_num) ; rm(CP2)
 
-rm(Obesity2, multi_mod1_1, mod, multi_mod2_2, multi_mod3_3, multi_mod4_4, multi_mod5_5, Predictions2)
+rm(Obesity_num, multi_mod1_1, mod, multi_mod2_2, multi_mod3_3, multi_mod4_4, multi_mod5_5, Predictions2)
 
-
-
-
-Obesity2 <- Obesity
-levels(Obesity$AgeGroup)
-Obesity2$agelin <- 0
-Obesity2$agelin[Obesity2$AgeGroup=="25-34"] <- 1
-Obesity2$agelin[Obesity2$AgeGroup=="35-44"] <- 2
-Obesity2$agelin[Obesity2$AgeGroup=="45-54"] <- 3
-Obesity2$agelin[Obesity2$AgeGroup=="55-64"] <- 4
-Obesity2$agelin[Obesity2$AgeGroup=="75+"] <- 5
-
-multi_mod1_1 <- multinom(BMIgroup ~ agelin + Employment + Sex + Fruit + Veg, data = Obesity2)
-mod <- step(multi_mod1_1)
-multi_mod2_2 <- multinom(BMIgroup ~ agelin + Employment + Sex + Fruit, data = Obesity2)
-multi_mod3_3 <- multinom(BMIgroup ~ agelin + Employment + Sex, data = Obesity2)
-multi_mod4_4 <- multinom(BMIgroup ~ agelin + Sex, data = Obesity2)
-multi_mod5_5 <- multinom(BMIgroup ~ agelin, data = Obesity2)
-
-# Predictions by Different Models
-Predictions2 <- data.frame(True_Values = Obesity2$BMIgroup, Multi_Mod1_1 = predict(multi_mod3_3),
-                           Multi_Mod2_2 = predict(multi_mod2_2), Multi_Mod3_3 = predict(multi_mod3_3),
-                           Multi_Mod4_4 = predict(multi_mod4_4), Multi_Mod5_5 = predict(multi_mod5_5))
 
 
 
--- Sample Analysis.R
+++ Sample Analysis.R
@@ -6,6 +6,11 @@ library(randomForest)
 library(ggthemes)
 library(sjPlot)
 
+
+# Reading Data
+Obesity <- read_csv("Obesity.csv")
+
+
 # Converting Variables into factor
 Obesity$AgeGroup <- as.factor(Obesity$AgeGroup)
 Obesity$Sex <- as.factor(Obesity$Sex)
@@ -21,6 +26,23 @@ Obesity$BMIgroup <- factor(x = Obesity$BMIgroup,
                            levels = c("Underweight", "Normal", "Overweight",
                                       "Obese"))
 levels(Obesity$BMIgroup)
+
+
+# Spiting data into "Train" and "Test" set
+Split = sample(x = c(TRUE, FALSE), size = nrow(Obesity), replace = TRUE,
+               prob = c(0.75, 0.25))
+Obesity_train = Obesity[Split, ]
+Obesity_test = Obesity[!Split, ]
+write_csv(Obesity_train, "D:/OneDrive - University of Glasgow/Study/Courses/Dissertation/R Analysis/MSc-Dissertation/Obesity_train.csv")
+write_csv(Obesity_test, "D:/OneDrive - University of Glasgow/Study/Courses/Dissertation/R Analysis/MSc-Dissertation/Obesity_test.csv")
+write_rds(Split, "D:/OneDrive - University of Glasgow/Study/Courses/Dissertation/R Analysis/MSc-Dissertation/Split.rds")
+rm(Obesity, Obesity_train, Obesity_test, Split) # Removing main data as not needed anymore
+
+
+# Reading Train and Test data
+# Split <- read_rds("Split.rds") # Train and Test indices, read if necessary
+Obesity_train <- read_csv("Obesity_train.csv")
+Obesity_test <- read_csv("Obesity_test.csv")
 
 
 # Proportion graphs
@@ -54,30 +76,83 @@ Prop_Year <- group_by(Obesity, Year, BMIgroup) %>% summarise(count=n()) %>%
   group_by(Year) %>% mutate(etotal=sum(count), proportion=count/etotal)
 ggplot(Prop_Year, aes(x=Year, y=proportion, group=BMIgroup, linetype=BMIgroup,
                       color=BMIgroup)) + geom_line()
+#Removing prop dataframes as there is no further use
+rm(Prop_Sex, Prop_AgeGroup, Prop_Emp, Prop_Fruit, Prop_Veg, Prop_Year)
 
 
 # Multinomial Model
 multi_mod1 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit + Veg,
-                       data = Obesity)
+                       data = Obesity_train)
 ## Model Selection
-null_mod <- multinom(BMIgroup ~ 1, data=Obesity)
+null_mod <- multinom(BMIgroup ~ 1, data=Obesity_train)
 setequal((null_mod$deviance - multi_mod1$deviance),
          (qchisq(p = 0.95,df = (multi_mod1$edf-null_mod$edf))))
 ## This shows that the null model is insignificant
-mod <- step(multi_mod1)
+mod <- step(multi_mod1) ; rm(mod)
 multi_mod2 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex + Fruit,
-                       data = Obesity) # Best Model
-multi_mod3 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity)
-multi_mod4 <- multinom(BMIgroup ~ AgeGroup + Sex, data = Obesity)
-multi_mod5 <- multinom(BMIgroup ~ AgeGroup, data = Obesity)
+                       data = Obesity_train) # Best Model
+multi_mod3 <- multinom(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity_train)
+multi_mod4 <- multinom(BMIgroup ~ AgeGroup + Sex, data = Obesity_train)
+multi_mod5 <- multinom(BMIgroup ~ AgeGroup, data = Obesity_train)
+
+
+
+
+# Checking the prediction values based on solely on intercept, which will give
+## prediction values of base categories:
+predict(multi_mod2, data.frame(AgeGroup = "16-24",
+                               Employment = "Doing something else",
+                               Sex = "Female", Fruit = "No"), type="probs")
+summary(multi_mod2)
+Alpha <- c(0, 2.966386, 1.809317, 1.524672) 
+## It can be done by following way:
+exp(Alpha)/sum(exp(Alpha)) ; rm(Alpha)
+
+
+# Slope term gives the log odds of moving from base category to next one which
+## can be seen using following command:
+Beta <- predict(multi_mod2, data.frame(AgeGroup = c("16-24", "25-34"),
+                                       Employment = c("Doing something else",
+                                                      "Doing something else"), 
+                          Sex = c("Female", "Female"), Fruit = c("No", "No")),
+                          type="probs") ; Beta.Pred
+exp(log(Beta[1,1]*Beta[2,2]/(Beta[1,2]*Beta[2,1]))) ; rm(Beta)
+## or we can just take the coefficient of AgeGroup "25-34" and take it's exp
+exp(0.7468219)
+## The odds of being Normal weight (versus "Underweight") for AgeGroup "25-34" &
+## considering all other covariates baseline is exp(0.74682) = 2.1102 times the
+## odds for "16-24" year old (multi_mod2). As the odds multiplier is greater
+## than "1", it means that younger people more tend to be "Underweight".
+### Confidence interval of odds multiplier: Coef +- 1.96 * SE(Coef)
+exp(c((0.7468219 - 1.96 * 0.3051748), (0.7468219 + 1.96 * 0.3051748))) # OR
+exp(confint(multi_mod2))
+### Plot showing variable significance and Confidence interval ranges:
+plot_model(multi_mod2, show.values = TRUE, title = "Odds", show.p = F)
+
+
+
+# Linear Discriminant Analysis
+lda_mod1 <- lda(BMIgroup ~ AgeGroup + Employment + Sex + Fruit + Veg,
+                data = Obesity_train) # Best Model
+lda_mod2 <- lda(BMIgroup ~ AgeGroup + Employment + Sex + Fruit,
+                data = Obesity_train)
+lda_mod3 <- lda(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity_train)
+
+## Prior probabilities are the default observed proportion in the data.
+## Here in Proportion of trace we can see that the first component is the most
+## dominating but the variation is not satisfactory so we take LD2 also for
+## classification. Age variable is dominated in LD1 and Employment variable is
+## dominated in LD2 and as seen before the classification will be done on the
+## basis of mainly first two LD1 & LD2.
 
 
 # Random_forest Model
 rf_mod1 <- randomForest(BMIgroup ~ AgeGroup + Employment + Sex + Fruit + Veg,
-                        data = Obesity) # Best Model
+                        data = Obesity_train)
 rf_mod2 <- randomForest(BMIgroup ~ AgeGroup + Employment + Sex + Fruit,
-                        data = Obesity)
-rf_mod3 <- randomForest(BMIgroup ~ AgeGroup + Employment + Sex, data = Obesity)
+                        data = Obesity_train)
+rf_mod3 <- randomForest(BMIgroup ~ AgeGroup + Employment + Sex,
+                        data = Obesity_train) # Best Model
 plot(rf_mod1)
 legend('topright', colnames(rf_mod1$err.rate), col=1:5, fill=1:5)
 ## The Plot shows that the prediction error rate of underweight is 100%,
@@ -99,70 +174,41 @@ ggplot(rankImportance, aes(x = reorder(Variables, Importance),
   geom_text(aes(x = Variables, y = 0.5, label = Rank),
             hjust=0, vjust=0.55, size = 4, colour = 'white') +
   labs(x = 'Variables') + coord_flip() + theme_excel_new()
+# Removing importance as there is no further use
+rm(importance, varImportance, rankImportance)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Predictions by Different Models
-Predictions <- data.frame(True_Values = Obesity$BMIgroup,
-                          Multi_Mod1 = predict(multi_mod1),
-                          Multi_Mod2 = predict(multi_mod2),
-                          Multi_Mod3 = predict(multi_mod3),
-                          Multi_Mod4 = predict(multi_mod4),
-                          Multi_Mod5 = predict(multi_mod5),
-                          Rf_Multi_Mod1 = predict(rf_mod1),
-                          Rf_Multi_Mod2 = predict(rf_mod2),
-                          Rf_Multi_Mod3 = predict(rf_mod3))
-
+Predictions <- tibble(True_Values = Obesity_test$BMIgroup,
+                      Multi_Mod2 = predict(multi_mod2, Obesity_test),
+                      Multi_Mod3 = predict(multi_mod3, Obesity_test),
+                      Multi_Mod4 = predict(multi_mod4, Obesity_test),
+                      Multi_Mod5 = predict(multi_mod5, Obesity_test),
+                      LDA_Mod1 = predict(lda_mod1, Obesity_test)$class,
+                      LDA_Mod2 = predict(lda_mod2, Obesity_test)$class,
+                      LDA_Mod3 = predict(lda_mod3, Obesity_test)$class,
+                      Rf_Multi_Mod1 = predict(rf_mod1, Obesity_test),
+                      Rf_Multi_Mod2 = predict(rf_mod2, Obesity_test),
+                      Rf_Multi_Mod3 = predict(rf_mod3, Obesity_test),)
 
 # Correct Classification rate
-CP <- xtabs( ~ Predictions$True_Values + Predictions$Multi_Mod2)
-(CP[1,1]+CP[2,2]+CP[3,3]+CP[4,4])/nrow(Obesity)
-## So according to multinomial model, the Model 2 is best.
-## According to Random_Forest, the Model 1 is best, will all variables.
+CP <- xtabs( ~ Predictions$True_Values + Predictions$Rf_Multi_Mod3) ; CP
+(CP[1,1]+CP[2,2]+CP[3,3]+CP[4,4])/nrow(Obesity_test) ; rm(CP)
 
-
-# Checking the prediction values based on solely on intercept, which will give
-## prediction values of base categories:
-predict(multi_mod2, data.frame(AgeGroup = "16-24",
-                               Employment = "Doing something else",
-                               Sex = "Female", Fruit = "No"), type="probs")
-Alpha_Pred <- c(0, 3.388426, 2.220982, 1.971101) 
-## It can be done by following way:
-exp(Alpha_Pred)/sum(exp(Alpha_Pred))
-
-
-# Slope term gives the log odds of moving from base category to next one which
-## can be seen using following command:
-Beta.Pred <- predict(multi_mod2, data.frame(AgeGroup = c("16-24", "25-34"),
-                      Employment = c("Doing something else",
-                                      "Doing something else"), 
-                      Sex = c("Female", "Female"), Fruit = c("No", "No")),
-                     type="probs") ; Beta.Pred
-exp(log(Beta.Pred[1,1]*Beta.Pred[2,2]/(Beta.Pred[1,2]*Beta.Pred[2,1])))
-## or we can just take the coefficient of AgeGroup "25-34" and take it's exp
-exp(0.6011892)
-## The odds of being Normal weight (versus "Underweight") for AgeGroup "25-34" &
-## considering all other covariates baseline is exp(0.6011) = 1.8241 times the
-## odds for "16-24" year old (multi_mod2). As the odds multiplier is greater
-## than "1", it means that younger people more tend to be "Underweight".
-### Confidence interval of odds multiplier: Coef +- 1.96 * SE(Coef)
-exp(c((0.6011892 - 1.96 * 0.2569298), (0.6011892 + 1.96 * 0.2569298)))
-### Plot showing variable significance and Confidence interval ranges:
-plot_model(multi_mod2, show.values = TRUE, title = "Odds", show.p = F)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Removing bad models and unnecessary predictions to save memory
+rm(null_mod, multi_mod1, multi_mod3, multi_mod4, multi_mod5, lda_mod2, lda_mod3, 
+   rf_mod1, rf_mod2, Predictions)
 
 
